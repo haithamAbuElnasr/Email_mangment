9efbff8160aac94582e2001fbb764e3b09d4f171
Haitham Mohamed <hm29825747@gmail.com>
2023-05-12 23:20:42 +0300

    Create home page

--- Email mangment/Form1.Designer.cs
+++ Email mangment/Form1.Designer.cs
@@ -28,35 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lableFileName = new System.Windows.Forms.Label();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.deleteFile = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // checkBox1
+            // panel1
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(68, 111);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(95, 20);
-            this.checkBox1.TabIndex = 0;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel1.Controls.Add(this.deleteFile);
+            this.panel1.Controls.Add(this.btnCreate);
+            this.panel1.Controls.Add(this.txtFileName);
+            this.panel1.Controls.Add(this.lableFileName);
+            this.panel1.Location = new System.Drawing.Point(1, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(797, 432);
+            this.panel1.TabIndex = 0;
+            // 
+            // lableFileName
+            // 
+            this.lableFileName.AutoSize = true;
+            this.lableFileName.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lableFileName.Location = new System.Drawing.Point(11, 101);
+            this.lableFileName.Name = "lableFileName";
+            this.lableFileName.Size = new System.Drawing.Size(205, 32);
+            this.lableFileName.TabIndex = 0;
+            this.lableFileName.Text = "Enter file name";
+            this.lableFileName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(222, 111);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(232, 22);
+            this.txtFileName.TabIndex = 1;
+            this.txtFileName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(509, 102);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(129, 41);
+            this.btnCreate.TabIndex = 2;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // deleteFile
+            // 
+            this.deleteFile.Location = new System.Drawing.Point(509, 169);
+            this.deleteFile.Name = "deleteFile";
+            this.deleteFile.Size = new System.Drawing.Size(129, 41);
+            this.deleteFile.TabIndex = 5;
+            this.deleteFile.Text = "Delete";
+            this.deleteFile.UseVisualStyleBackColor = true;
+            this.deleteFile.Click += new System.EventHandler(this.deleteFile_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.checkBox1);
+            this.ClientSize = new System.Drawing.Size(800, 433);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lableFileName;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button deleteFile;
     }
 }
 
